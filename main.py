31ef132fc0d51245e4366195063afbb2f5f3a342
adicha <bolotovakadicha@icloud.com>
2024-11-25 12:23:38 +0600

    First input

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import random
 locations = [random.randint(1, 7) for _ in range(3)]
 print("Guess the locations of the 3 boxes and their weights. Enter one location and its weight at a time.")
+while True:
+    print("Enter the first location and weight:")
+    guess1 = int(input())
+    weight1 = int(input())
