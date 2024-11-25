190d48d19a1e4067b989f189755f8b28a67e2a3c
adicha <bolotovakadicha@icloud.com>
2024-11-25 12:28:04 +0600

    guesses count

--- main.py
+++ main.py
@@ -12,4 +12,7 @@ while True:
     guess3 = int(input())
     weight3 = int(input())
     if guess1 != guess2 and guess1 != guess3 and guess2 != guess3:
-        break
+        correct_guesses = 0
+        for g in [guess1, guess2, guess3]:
+            if g in locations:
+                correct_guesses += 1
